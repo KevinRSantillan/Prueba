5932f00d547674b26a39ed08f578445619bc7c76
KevinRSantillan <63812607+KevinRSantillan@users.noreply.github.com>
2020-09-23 16:52:01 -0500

    prueba 1

---
+++ resources_demo-1/src/main/java/com/resources/ResourcesDemo1Application.java
@@ -0,0 +1,13 @@
+package com.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesDemo1Application.class, args);
+	}
+
+}
---
+++ resources_demo-1/src/test/java/com/resources/ResourcesDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
